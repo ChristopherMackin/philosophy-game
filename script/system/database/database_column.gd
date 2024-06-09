fc3b445e95c13ffc06f372b84f49272c2239c193
ChristopherMackin <christopher.daniel.mackin@gmail.com>
2024-06-09 17:11:30 -0500

    Made db column type

---
+++ script/system/database/database_column.gd
@@ -0,0 +1,7 @@
+extends Resource
+
+class_name DatabaseColumn
+
+@export var key : String
+@export var type : Variant.Type
+@export_multiline var description : String
--- script/system/database/state_database.gd
+++ script/system/database/state_database.gd
@@ -3,33 +3,37 @@ extends Resource
 
 class_name StateDatabase
 
-@export var keys : Array[String]:
-	get: return keys
-	set(value):
-		var keys_to_add = difference(value, keys)
-		for key in keys_to_add:
-			_dictionary[key] = null
-		
-		var keys_to_remove = difference(keys, value)
-		for key in keys_to_remove:
-			_dictionary.erase(key)
-		keys = value
+@export var columns : Array[DatabaseColumn]
 
 @export var _dictionary : Dictionary
 
 var value : Dictionary:
 	get: return _dictionary.duplicate()
 
-func update(key : String, value):
-	if _dictionary.has(key):
-		_dictionary[key] = value
+func update(key : String, value) -> bool:
+	for col in columns:
+		if col.key == key:
+			if typeof(value) == col.type:
+				_dictionary[key] = value
+				return true
+			else:
+				return false
+	
+	return false
+
+func clean_up():
+	var col_keys = columns.map(func(x): return x.key)
+	var dic_keys = _dictionary.keys
+	
+	var keys_to_remove = difference(dic_keys, col_keys)
+	
+	for k in keys_to_remove:
+		_dictionary.erase(k)
 
 func clear():
 	_dictionary.clear()
-	for k in keys:
-		_dictionary[k] = null
 
-static func difference(arr1 : Array, arr2 : Array):
+func difference(arr1, arr2):
 	var only_in_arr1 = []
 	for v in arr1:
 		if not (v in arr2):
