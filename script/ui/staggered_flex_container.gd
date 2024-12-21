722d5f1334569f60cbafabbffe2eaad1bad08cac
Christopher Sartin <christopher.sartin@ChristophersMBP.attlocal.net>
2024-12-21 00:28:37 -0600

    Got staggered container horizontal spacing finished

--- script/ui/staggered_flex_container.gd
+++ script/ui/staggered_flex_container.gd
@@ -4,6 +4,15 @@ extends Container
 
 class_name StaggeredFlexContainer
 
+@export var horizontal_spacing : int:
+	set(val):
+		horizontal_spacing = val if val >= 0 else 0
+		queue_sort()
+@export var perferred_vertical_spacing : int:
+	set(val):
+		perferred_vertical_spacing = val if val >= 0 else 0
+		queue_sort()
+
 func _notification(what):
 	if what == NOTIFICATION_CHILD_ORDER_CHANGED:
 		queue_sort()
@@ -15,7 +24,40 @@ func _sort_children():
 		return
 	
 	var children = get_children()
-	var max_x = children.map(func(x): return x.custom_minimum_size.x).max()
-	var max_y = children.map(func(x): return x.custom_minimum_size.y).max()
+	var max_x : int = 0
+	var max_y : int = 0
 	
-	print("%s,%s" %[max_x, max_y])
+	#Get max minimum_custom_size
+	for child in children:
+		var custom_min = child.custom_minimum_size
+		custom_min.x *= child.scale.x
+		custom_min.y *= child.scale.y
+		
+		max_x = custom_min.x \
+		if custom_min.x > max_x \
+		else max_x
+		
+		max_y = custom_min.y \
+		if custom_min.y > max_y \
+		else max_y
+	
+	#projected elements per row
+
+	var elements_per_row : int = floor(size.x / (max_x + horizontal_spacing))
+	
+	var index = 0
+	for child in children:
+		var col : int = index % elements_per_row
+		var row : int = floor(index / elements_per_row)
+		var is_odd := bool(row % 2)
+		
+		if is_odd:
+			child.position = Vector2(\
+			size.x - (max_x + horizontal_spacing + ((max_x + horizontal_spacing) * (col))),\
+			max_y * floor(row))
+		else:
+			child.position = Vector2(\
+			horizontal_spacing + ((max_x + horizontal_spacing) * (col)),\
+			max_y * floor(row))
+		
+		index += 1
