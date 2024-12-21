c84a931f78e296288a9ff2fb8fa6c8a53cfdc361
ChristopherMackin <christopher.daniel.mackin@gmail.com>
2024-12-20 23:13:53 -0600

    Working on card container UI

--- script/ui/staggered_flex_container.gd
+++ script/ui/staggered_flex_container.gd
@@ -8,7 +8,14 @@ func _notification(what):
 	if what == NOTIFICATION_CHILD_ORDER_CHANGED:
 		queue_sort()
 	if what == NOTIFICATION_SORT_CHILDREN:
-		# Must re-sort the children
-		for c in get_children():
-			# Fit to own size
-			fit_child_in_rect(c, Rect2(Vector2(), size))
+		_sort_children()
+
+func _sort_children():
+	if get_child_count() <= 0:
+		return
+	
+	var children = get_children()
+	var max_x = children.map(func(x): return x.custom_minimum_size.x).max()
+	var max_y = children.map(func(x): return x.custom_minimum_size.y).max()
+	
+	print("%s,%s" %[max_x, max_y])
