c635bf15320e22da77614fcb396834af76c6b26f
Christopher Sartin <christopher.sartin@ChristophersMBP.attlocal.net>
2025-02-07 14:32:44 -0600

    Added in deck/hand removal addition

--- script/system/deck/deck.gd
+++ script/system/deck/deck.gd
@@ -38,3 +38,26 @@ func remove_from_deck(top : Top):
 	composition_top_deck_config_array[index].count -= 1
 	if composition_top_deck_config_array[index].count <= 0:
 		composition_top_deck_config_array.remove_at(index)
+
+func add_to_deck(top : Top):
+	var tops = composition_top_deck_config_array.map(func(x : TopsDeckConfig): return x.top_data)
+	var index = tops.find(top.data)
+	
+	if index < 0:
+		composition_top_deck_config_array.append(TopsDeckConfig.new(top.data))
+		index = composition_top_deck_config_array.size() - 1
+	
+	composition_top_deck_config_array[index].count += 1
+
+func remove_from_draw_pile(top : Top):
+	var tops_data = draw_pile.filter(func(x): return x.top_data)
+	var index = tops_data.find(top.data)
+	
+	if index < 0: return
+	
+	draw_pile.remove_at(index)
+	draw_pile.shuffle()
+
+func add_to_draw_pile(top : Top):
+	draw_pile.append(top)
+	draw_pile.shuffle()
--- script/system/top/tops_deck_config.gd
+++ script/system/top/tops_deck_config.gd
@@ -4,3 +4,7 @@ class_name TopsDeckConfig
 
 @export var top_data : TopData
 @export var count : int
+
+func _init(top_data : TopData = null, count : int = 0):
+	self.top_data = top_data
+	self.count = count
