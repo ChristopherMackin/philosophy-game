500a21bd8bcf5caabf585ec2c12f9fa25483b87b
Christopher Sartin <christopher.sartin@ChristophersMBP.attlocal.net>
2025-04-12 23:31:28 -0500

    Starting dynamic board

--- script/system/card/card_action/store_card_collection_suits_in_blackboard_card_action.gd
+++ script/system/card/card_action/store_card_collection_suits_in_blackboard_card_action.gd
@@ -8,6 +8,9 @@ class_name StoreCardCollectionSuitsInBlackboardCardAction
 func invoke(caller : Card, player : Contestant, manager : DebateManager) -> bool:
 	collection_container.init(caller, player, manager)
 	var cards = await collection_container.get_collection_cards()
+	
+	if cards.size() <= 0: return true
+	
 	var suits = cards.map(func(card): return card.suit)
 	
 	manager.blackboard.add("action_%s" % key, suits, Blackboard.ExpirationToken.ON_ACTION_END)
