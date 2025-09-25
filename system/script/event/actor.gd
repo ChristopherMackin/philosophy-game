58e915724f2f0a2755999614930c8b36f0001832
Christopher Sartin <christopher.sartin@ChristophersMBP.attlocal.net>
2025-09-24 21:56:25 -0500

    Changed language to actors

---
+++ system/script/event/actor.gd
@@ -0,0 +1,9 @@
+extends Node
+
+class_name Actor
+
+@export var dialogue_display_name: String
+@export var dialogue_area_override: DialogueArea
+
+func focus_actor(val: bool):
+	pass
--- system/script/event/event_subscriber/scene_event_manager.gd
+++ system/script/event/event_subscriber/scene_event_manager.gd
@@ -3,20 +3,7 @@ extends EventSubscriber
 class_name SceneEventManager
 
 @export var scene_animator_handler: AnimationHandler
-@export var animated_characters: Array[AnimatedCharacter]
-var active_speaker: AnimatedCharacter:
-	get:
-		return active_speaker
-	set(val):
-		if active_speaker:
-			active_speaker.layer_mask.set_layer(20, false)
-			active_speaker.dialgoue_camera.priority = 0
-		
-		active_speaker = val
-		
-		if active_speaker:
-			active_speaker.layer_mask.set_layer(20, true)
-			active_speaker.dialgoue_camera.priority = 1
+@export var actors: Array[Actor]
 
 @export var default_dialogue_area: DialogueArea
 
@@ -24,8 +11,8 @@ func _ready():
 	super._ready()
 	if default_dialogue_area:
 		default_dialogue_area.visible = false
-	for animated_character in animated_characters:
-		if animated_character.dialogue_area_override: animated_character.dialogue_area_override.visible = false
+	for actor in actors:
+		if actor.dialogue_area_override: actor.dialogue_area_override.visible = false
 
 func display_dialogue(line : String, actor : String, await_input : bool, seconds_before_close : float):
 	var index = get_actor_index(actor)
@@ -33,28 +20,28 @@ func display_dialogue(line : String, actor : String, await_input : bool, seconds
 		push_error("MISSING LINE \nACTOR: \"%s\"\n LINE: %s" % [actor, line])
 		return
 	
-	var animated_character = animated_characters[index]
-	var dialogue_area = default_dialogue_area if animated_character.dialogue_area_override == null else animated_character.dialogue_area_override
+	var current_actor = actors[index]
+	var dialogue_area = default_dialogue_area if current_actor.dialogue_area_override == null else current_actor.dialogue_area_override
 	
-	active_speaker = animated_character
+	current_actor.focus_actor(true)
 	dialogue_area.visible = true
-	dialogue_area.set_text(line, actor)
-	animated_character.is_talking = true
+	dialogue_area.set_text(line, current_actor.dialogue_display_name)
+	current_actor.is_talking = true
 	
 	await dialogue_area.on_dialogue_finished
 	
-	animated_character.is_talking = false
+	current_actor.is_talking = false
 	await GlobalTimer.wait_for_seconds(seconds_before_close)
 	dialogue_area.visible = false
-	active_speaker = null
+	current_actor.focus_actor(false)
 
 func cancel_dialogue(actor):
 	var index = get_actor_index(actor)
-	var animated_character = animated_characters[index]
-	var dialogue_area = default_dialogue_area if animated_character.dialogue_area_override == null else animated_character.dialogue_area_override
+	var current_actor = actors[index]
+	var dialogue_area = default_dialogue_area if current_actor.dialogue_area_override == null else current_actor.dialogue_area_override
 
 	dialogue_area.visible = false
-	active_speaker = null
+	current_actor.focus_actor(false)
 
 func play_animation(animation : String, actor : String, await_animation : bool):
 	var parent
@@ -66,7 +53,7 @@ func play_animation(animation : String, actor : String, await_animation : bool):
 		if index < 0:
 			return
 			
-		parent = animated_characters[index]
+		parent = actors[index]
 		animation_handler = parent.get_node_or_null(NodePath("AnimationHandler"))
 
 	
@@ -91,7 +78,7 @@ func cancel_animation(actor):
 		if index < 0:
 			return
 			
-		parent = animated_characters[index]
+		parent = actors[index]
 		animation_handler = parent.get_node_or_null(NodePath("AnimationHandler"))
 
 	
@@ -101,4 +88,4 @@ func cancel_animation(actor):
 	animation_handler.cancel_animation()
 
 func get_actor_index(actor_name : String) -> int:
-	return animated_characters.map(func(x): return x.name.to_snake_case()).find(actor_name.to_snake_case())
+	return actors.map(func(x): return x.name.to_snake_case()).find(actor_name.to_snake_case())
