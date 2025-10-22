14b6b8a7b1898c89dfc7c8a507111ce072c6c9c4
Christopher Sartin <christopher.sartin@Christophers-MacBook-Pro.local>
2025-10-22 16:18:41 -0500

    Working on fleshing out scene transitions

--- cutscene/scene_change_node.gd
+++ cutscene/scene_change_node.gd
@@ -1,7 +1,9 @@
-extends Node3D
+extends Node
 
-@export var transition: PackedScene
+class_name SceneChangeNode
+
 @export var scene_name: String
+@export var transition: PackedScene
 
 func progress():
 	SceneManager.replace_scene_async(scene_name, transition)
--- system/script/event/task_action/set_global_value_task_action.gd
+++ system/script/event/task_action/set_global_value_task_action.gd
@@ -3,11 +3,10 @@ extends TaskAction
 class_name SetGlobalValueTaskAction
 
 func invoke(task : Task, manager : EventManager):
-	var path = task.get_input(0)
 	var bb : Blackboard = GlobalBlackboard.blackboard
 	
-	bb.add(task.get_input(1), task.get_input(2))
+	bb.add(task.get_input(0), task.get_input(1))
 	
-	ResourceSaver.save(bb, path)
+	#ResourceSaver.save(bb, path)
 	
 	on_action_complete.emit(task.get_output(0))
