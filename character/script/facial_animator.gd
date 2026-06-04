75babe4fb2f65416aaa8ad2b3d8e3e9dc4c04c29
Christopher Sartin <christopher.sartin@Christophers-MacBook-Pro.local>
2026-06-04 00:03:12 -0500

    Removing assets

--- character/script/facial_animator.gd
+++ character/script/facial_animator.gd
@@ -6,20 +6,14 @@ class_name FacialAnimator
 @export_group("Eyes")
 @export var eye_col_count: int = 4
 @export var eye_row_count: int = 4
-@export var eyes : MeshInstance3D
+@export var eyes_meshes : Array[MeshInstance3D] = []
 @export var eye_selector_bone_name: String = "eye_selector"
-var eye_material : Material:
-	get:
-		return eyes.mesh.surface_get_material(0)
 
 @export_group("Mouth")
 @export var mouth_col_count: int = 4
 @export var mouth_row_count: int = 4
-@export var mouth : MeshInstance3D
+@export var mouth_meshes : Array[MeshInstance3D]
 @export var mouth_selector_bone_name: String = "mouth_selector"
-var mouth_material : Material:
-	get():
-		return mouth.mesh.surface_get_material(0)
 
 @export_group("Skeleton")
 @export var skeleton : Skeleton3D
@@ -31,23 +25,38 @@ func pose_updated():
 func update_eyes():
 	var id = skeleton.find_bone(eye_selector_bone_name)
 	var t = skeleton.get_bone_pose(id)
+	var r = skeleton.get_bone_rest(id)
+	var origin = (t.origin - r.origin).clamp(Vector3.ZERO, Vector3.ONE)
 	
 	var offset = Vector3(
-		float(ceil(clamp(abs(t.origin.x), 0, 1) * eye_col_count) - 1) / eye_col_count,
-		float(ceil(clamp(abs(t.origin.y), 0, 1) * eye_row_count) - 1) / eye_row_count,
+		float(ceil(max(origin.x * eye_col_count, 1)) - 1) / eye_col_count,
+		float(ceil(max(origin.y * eye_row_count, 1)) - 1) / eye_row_count,
 		0
 	)
 	
-	eye_material.uv1_offset = offset
+	for m in eyes_meshes:
+		var material = m.mesh.surface_get_material(0)
+		if material is ShaderMaterial:
+			material.set_shader_parameter("uv_offset", offset)
+		else:
+			material.uv1_offset = offset
+
 
 func update_mouth():
 	var id = skeleton.find_bone(mouth_selector_bone_name)
 	var t = skeleton.get_bone_pose(id)
+	var r = skeleton.get_bone_rest(id)
+	var origin = (t.origin - r.origin).clamp(Vector3.ZERO, Vector3.ONE)
 	
 	var offset = Vector3(
-		float(ceil(clamp(abs(t.origin.x), 0, 1) * mouth_col_count) - 1) / eye_col_count,
-		float(ceil(clamp(abs(t.origin.y), 0, 1) * mouth_row_count) - 1) / mouth_row_count,
+		float(ceil(max(origin.x * mouth_col_count, 1)) - 1) / mouth_col_count,
+		float(ceil(max(origin.y * mouth_row_count, 1)) - 1) / mouth_row_count,
 		0
 	)
 	
-	mouth_material.uv1_offset = offset
+	for m in mouth_meshes:
+		var material = m.mesh.surface_get_material(0)
+		if material is ShaderMaterial:
+			material.set_shader_parameter("uv_offset", offset)
+		else:
+			material.uv1_offset = offset
---
+++ character/script/outfit_skeleton_target.gd
@@ -0,0 +1,32 @@
+@tool
+extends Node3D
+
+class_name OutfitSkeletonTarget
+
+var skeleton: Skeleton3D:
+	get:
+		if skeleton == null:
+			var skeletons = find_children("*", "Skeleton3D", true)
+			if not skeletons.is_empty():
+				skeleton = skeletons[0]
+				
+		return skeleton
+
+@export var target: Skeleton3D:
+	set(val):
+		if target and target.pose_updated.is_connected(_pose_updated):
+			target.pose_updated.disconnect(_pose_updated)
+		
+		target = val
+		
+		if target and not target.pose_updated.is_connected(_pose_updated):
+			target.pose_updated.connect(_pose_updated)
+
+func _pose_updated():
+	if !skeleton || !target: return
+	
+	for i in skeleton.get_bone_count():
+		# Match the custom pose transforms from the animated master
+		skeleton.set_bone_pose_position(i, target.get_bone_pose_position(i))
+		skeleton.set_bone_pose_rotation(i, target.get_bone_pose_rotation(i))
+		skeleton.set_bone_pose_scale(i, target.get_bone_pose_scale(i))
