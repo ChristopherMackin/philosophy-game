82b81b59e7559412a2cbc7a591146d72cad8a4f3
Christopher Mackin <christopher.daniel.sartin@gmail.com>
2026-09-19 16:56:58 -0500

    Made it so computers won't try to hold cards

--- character/script/brain/card_brain/bucket_card_brain.gd
+++ character/script/brain/card_brain/bucket_card_brain.gd
@@ -5,6 +5,12 @@ class_name BucketCardBrain
 @export var brains: Array[CardBrain]
 
 func select(contestant: Contestant, request : SelectionRequest) -> SelectionResponse:
+	if contestant == contestant.manager.computer:
+		#TODO: Currently this stops computer from using the hold. 
+		#I don't think this is a bad options, but this will have 
+		#to be changed if allowing computer hold card is needed.
+		request.options = contestant.playable_cards
+	
 	for brain in brains:
 		var selection = brain.select(contestant, request)
 		if selection:
