042e6bb8b254ac93cb57a0359785ef44771544f3
ChristopherMackin <christopher.daniel.mackin@gmail.com>
2026-09-23 12:02:28 -0500

    Adding more cards

---
+++ system/script/card/card_action/modify_energy_per_token_counter_card_action.gd
@@ -0,0 +1,16 @@
+extends CardAction
+
+class_name ModifyEnergyPerTokenCounterCardAction
+
+@export var which_contestant : Const.WhichContestant
+@export var amount: int
+@export var operation: EnumMath.Operation
+
+func invoke(caller : Card, player : Contestant, manager : DebateManager) -> bool:
+	var contestant := Const.get_contestant(player, manager.get_opponent(player), which_contestant)
+	
+	contestant.current_energy = EnumMath.evaluate(contestant.current_energy, amount * caller.token_counter, operation)
+	if contestant.current_energy < 0:
+		contestant.current_energy = 0
+	
+	return true
